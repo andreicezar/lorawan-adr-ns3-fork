d63ebd9252e457181a9832708fb634fb0369871c
andreicezar <48317796+andreicezar@users.noreply.github.com>
2025-06-27 00:14:54 +0300

    adropt starting to work a little bit

--- ns-3.42/cmake-cache/examples/channel-models/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrei/ns-allinone-3.42/ns-3.42/examples/channel-models
-# Build directory: /home/andrei/ns-allinone-3.42/ns-3.42/cmake-cache/examples/channel-models
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ctest-three-gpp-v2v-channel-example "ns3.42-three-gpp-v2v-channel-example-debug")
-set_tests_properties(ctest-three-gpp-v2v-channel-example PROPERTIES  WORKING_DIRECTORY "/home/andrei/ns-allinone-3.42/ns-3.42/build/examples/channel-models/" _BACKTRACE_TRIPLES "/home/andrei/ns-allinone-3.42/ns-3.42/build-support/custom-modules/ns3-executables.cmake;47;add_test;/home/andrei/ns-allinone-3.42/ns-3.42/build-support/custom-modules/ns3-executables.cmake;140;set_runtime_outputdirectory;/home/andrei/ns-allinone-3.42/ns-3.42/build-support/macros-and-definitions.cmake;1424;build_exec;/home/andrei/ns-allinone-3.42/ns-3.42/examples/channel-models/CMakeLists.txt;1;build_example;/home/andrei/ns-allinone-3.42/ns-3.42/examples/channel-models/CMakeLists.txt;0;")
--- ns-3.42/cmake-cache/examples/stats/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrei/ns-allinone-3.42/ns-3.42/examples/stats
-# Build directory: /home/andrei/ns-allinone-3.42/ns-3.42/cmake-cache/examples/stats
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ctest-wifi-example-sim "ns3.42-wifi-example-sim-debug")
-set_tests_properties(ctest-wifi-example-sim PROPERTIES  WORKING_DIRECTORY "/home/andrei/ns-allinone-3.42/ns-3.42/build/examples/stats/" _BACKTRACE_TRIPLES "/home/andrei/ns-allinone-3.42/ns-3.42/build-support/custom-modules/ns3-executables.cmake;47;add_test;/home/andrei/ns-allinone-3.42/ns-3.42/build-support/custom-modules/ns3-executables.cmake;140;set_runtime_outputdirectory;/home/andrei/ns-allinone-3.42/ns-3.42/build-support/macros-and-definitions.cmake;1424;build_exec;/home/andrei/ns-allinone-3.42/ns-3.42/examples/stats/CMakeLists.txt;1;build_example;/home/andrei/ns-allinone-3.42/ns-3.42/examples/stats/CMakeLists.txt;0;")
